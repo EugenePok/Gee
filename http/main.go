58a1c16a4fd4b1f0e693ce4ada596bc5b33a718d
Eugene Pok <zcpok97@gmail.com>
2026-05-30 00:06:21 +0800

    update main.go for trie router

--- http/main.go
+++ http/main.go
@@ -11,6 +11,8 @@ func main() {
 	c := gee.New()
 	c.GET("/", indexHandler)
 	c.GET("/hello", helloHandler)
+	c.GET("/hello/:name", helloWithParamsHandler)
+	c.GET("/assets/*filepath", assetHandler)
 	c.POST("/login", loginHandler)
 	log.Fatal(c.Run(":9999"))
 }
@@ -23,9 +25,17 @@ func helloHandler(c *gee.Context) {
 	c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 }
 
+func helloWithParamsHandler(c *gee.Context) {
+	c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+}
+
 func loginHandler(c *gee.Context) {
 	c.JSON(http.StatusOK, gee.H{
 		"username": c.PostForm("username"),
 		"password": c.PostForm("password"),
 	})
+}
+
+func assetHandler(c *gee.Context) {
+	c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
 }
